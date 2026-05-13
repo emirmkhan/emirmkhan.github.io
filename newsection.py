33e9550991d7e6fb1d1507dcf2a6f3069967f03e
emirmkhan <emir02212017@gmail.com>
2026-05-12 19:42:24 -0700

    Delete newsection.py
    Signed-off-by: emirmkhan <emir02212017@gmail.com>

--- newsection.py
+++
@@ -1,6 +0,0 @@
-passcode=input("Passcode")
-if passcode=="386492":
-    print("Comments to Subhaan Saad Khan and Maheen Naz Quraishi")
-passcode=input("Passcode")
-if passcode<>"386492":
-    print("Error")
