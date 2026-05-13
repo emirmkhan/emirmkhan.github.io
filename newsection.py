1d0304841b8f36227938a920650c4eb957d93a46
emirmkhan <emir02212017@gmail.com>
2026-05-12 19:16:34 -0700

    Add passcode validation in newsection.py
    Signed-off-by: emirmkhan <emir02212017@gmail.com>

---
+++ newsection.py
@@ -0,0 +1,6 @@
+passcode=input("Passcode")
+if passcode=="386492":
+    print("Comments to Subhaan Saad Khan and Maheen Naz Quraishi")
+passcode=input("Passcode")
+if passcode<>"386492":
+    print("Error")
